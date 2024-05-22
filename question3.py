15aafa7965d5c978b69cb18838eddad28a27cf02
GoforthWalker <154067732+GoforthWalker@users.noreply.github.com>
2024-05-22 11:05:14 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,13 @@
+num = int(input('Enter the num value:'))
+factorial = 1
+
+if num < 0:
+    print("num is negative")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+
+    print(f"The factorial of {num} is {factorial}")
 #write a program to find the factorial of a nummber
