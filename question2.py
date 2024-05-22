78889763c7db1ea7da11b9137e5237b4b9c4866b
GoforthWalker <154067732+GoforthWalker@users.noreply.github.com>
2024-05-22 10:57:38 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,1 +1,9 @@
+def is_palindrome(n):
+    return str(n) == ''.join(reversed(str(n)))
+
+n = int(input("Enter an integer: "))
+if is_palindrome(n):
+    print(f"{n} is a palindrome.")
+else:
+    print(f"{n} is not a palindrome.")
 # find if the given number is a palindrome or not?
