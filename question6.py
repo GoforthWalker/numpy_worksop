2f1a5e77dce2f7bd7d4d14155cf07e629385ac76
GoforthWalker <154067732+GoforthWalker@users.noreply.github.com>
2024-05-22 11:35:29 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,8 @@
+A=int(input('Enter the A value:'))
+B=int(input('Enter the B value:'))
+
+if a>b:
+    print(a,'is a maximum no')
+else:
+    print(b,'is a maximum no')
 #write a program to find the maximum of two numbers
