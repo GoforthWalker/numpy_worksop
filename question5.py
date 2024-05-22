56c92995f2de76c3ec86ad00aacbad4fbc01cb84
GoforthWalker <154067732+GoforthWalker@users.noreply.github.com>
2024-05-22 11:29:46 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,1 +1,13 @@
+num = int(input('Enter the num value:')) 
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(f"{num} is not a prime number")
+            print(f"{i} times {num // i} is {num}")
+            break
+    else:
+        print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
 #write a program to find if the given number is prime no or not
