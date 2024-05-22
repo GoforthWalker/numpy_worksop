ef5009c74e20b2f853e06494cbcd7fdbf011bf36
GoforthWalker <154067732+GoforthWalker@users.noreply.github.com>
2024-05-22 11:22:38 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,6 @@
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
+n = int(input('Enter the no:'))
+print(f"Sum of digits: {sum_of_digits(n)}")
 #write a program to find the sum of digits of a given number'
